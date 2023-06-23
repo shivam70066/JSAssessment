2e0a21658d26e4fadce6e632cc6f356b509e2862
shivam70066 <137193105+shivam70066@users.noreply.github.com>
2023-06-23 16:36:52 +0530

    Update JSTest.js

--- JSTest.js
+++ JSTest.js
@@ -13,26 +13,29 @@ const NFTs=[];
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT (_name,_age,_uid) {
+function mintNFT (_name,_uid,color,size) {
     const newNFT={
         "name" : _name,
-        age : _age,
-        uniqueID : _uid
+        uniqueID : _uid,
+        "color" : color,
+        "size" : size
     }
     NFTs.push(newNFT);
-    console.log("Minted : " + _name);
+    console.log("Minting " + _name + "'s NFT..");
+    console.log("Successfully Minted\n");
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {
     for(let i =0; i<NFTs.length; i++){
-        console.log("\n"+NFTs[i].name+"'s Details");
+        console.log(""+NFTs[i].name+"'s NFT Details");
         console.log("UID  : "+NFTs[i].uniqueID);
-        console.log("Name : "+NFTs[i].name);
-        console.log("Age  : "+NFTs[i].age);
-        
+        console.log("Name : "+NFTs[i].name);   
+        console.log("Color  : "+NFTs[i].color);     
+        console.log("Size  : "+NFTs[i].size + "\n");     
     }
+    console.log("All NFTs printed....")
 }
 
 // print the total number of NFTs we have minted to the console
@@ -41,7 +44,9 @@ function getTotalSupply() {
 }
 
 // call your functions below this line
-mintNFT("Shivam",23,20112);
-mintNFT("Rahul",22,20110);
+mintNFT("Shivam",23,"red","small");
+mintNFT("Rahul",22,"black","large");
+mintNFT("Sourav",29,"blue","average");
+mintNFT("Siya",31,"black","extra large");
 listNFTs();
 getTotalSupply();
